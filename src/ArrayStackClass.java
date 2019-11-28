5401ac66a2aa7d10f8147e6d41661df8e5897c50
Cui <ArsenYiJing.Cui@student.tdsb.on.ca>
2019-11-28 09:59:18 -0500

    Adds tips

--- src/ArrayStackClass.java
+++ src/ArrayStackClass.java
@@ -1,6 +1,43 @@
 
-public class ArrayStackClass {
+public class ArrayStackClass<T> implements ArrayStack<T> {
+	
+	private Node<T>[] list;
+	
+	public ArrayStackClass(int n)
+	{
+		list = new Node[n];
+	}
+	
+	@Override
+	public void push(Node<T> n) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public Node<T> pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node<T> peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
